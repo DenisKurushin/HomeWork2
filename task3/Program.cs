ed19b2f3113aa614ad8686707497a0cd7360361b
Denis Kurushin <deniskurushin@MacBook-Air-Denis.local>
2022-12-12 11:49:41 +0300

    tasks done

---
+++ task3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите число");
+int N1 =  Convert.ToInt32 (Console.ReadLine()); 
+
+
+if ( N1 % 23 == 0)
+{
+    if ( N1 % 7 == 0)
+    Console.WriteLine("число кратно 23 и 7");
+}
+else
+{
+    Console.WriteLine("не кратно");
+}
